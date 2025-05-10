1eee4e1ea0c9b96e935295df2a2b1f097230220a
Richard Wasonga <rowwasonga@gmail.com>
2025-05-10 10:51:49 +0300

    complete lab

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,15 @@
 # your code goes here!
+class Anagram:
+
+    def __init__(self,word):
+        self.word = word.lower()
+
+    def match(self,word_list):
+        sorted_word = sorted(self.word)
+
+        matches = []
+        for words in word_list:
+            if sorted(words.lower()) == sorted_word and words.lower() != self.word:
+                matches.append(words)
+
+        return matches
